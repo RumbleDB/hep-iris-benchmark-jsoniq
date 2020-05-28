5310204ee495692c6285a3d5c1dc03d25f87abb1
DanGraur <dan.graur@inf.ethz.ch>
2020-05-28 16:14:52 +0200

    Added a script for running Ruble jobs in debug mode.

---
+++ scripts/bash/run_job_debug.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Set the spark submit
+spark=/home/dan/data/software/spark-2.4.5-bin-hadoop2.7/bin/spark-submit
+# rumble=/home/dan/data/software/rumble/spark-rumble-1.5.jar
+# rumble=/home/dan/data/software/rumble/spark-rumble-1.6.2.jar
+rumble=/home/dan/data/software/rumble/spark-rumble-1.6.3.jar
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Run the job
+if [[ $# -ne 1 ]]
+then
+	echo "Usage: run_job.sh <rumble_source>"
+	echo "Running Rumble in interactive mode"
+	$spark $rumble --show-error-info yes --shell yes	
+else
+	echo "Submitting Ruble query located at $1"
+	$spark $rumble --show-error-info yes --query-path $1 # --output-path "hdfs:///user/me/results/output" --log-path "hdfs:///user/me/logging/mylog"
+fi
+
+
